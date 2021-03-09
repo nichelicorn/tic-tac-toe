1ebf2fe9af047483cd03823e6358a6c57cfff11b
Nichele Dunn <the7yearitch@hotmail.com>
2021-03-08 17:11:16 -0700

    Set to local storage working for player1 and player2

--- src/player.js
+++ src/player.js
@@ -10,8 +10,16 @@ class Player {
     // console.log("player1?", game.player1);
     // console.log("player2?", game.player2);
 
-    // var winsToStore = game.Player;
-    // this method will save Player object to localStorage
+    // var winsToStore = this.Player; // logs undefined
+    var p1WinsToStore = game.player1;
+    var p2WinsToStore = game.player2;
+    // console.log("localStorage...winsToStore:", winsToStore); // logs the currentPlayer object
+    // console.log("winsToStore:", winsToStore);
+    var stringifiedP1WinsToStore = JSON.stringify(p1WinsToStore);
+    var stringifiedP2WinsToStore = JSON.stringify(p2WinsToStore);
+    // console.log("stringifiedWinsToStore:", stringifiedWinsToStore);
+    localStorage.setItem("winner1Object", stringifiedP1WinsToStore);
+    localStorage.setItem("winner2Object", stringifiedP2WinsToStore);
   }
 
   retrieveWinsFromStorage() {
